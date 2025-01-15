5786ce074bfc8dd3c3b9835388faea4378ebc2c0
tsusunt <takasyah1@gmail.com>
2025-01-16 01:40:12 +0700

    pagi

---
+++ frontend/src/components/UserList.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const UserList = () => {
+  return (
+    <div className="columns mt-5">
+        <div className="column is-half">Lorem ipsum dolor sit amet.</div>
+    </div>
+  )
+}
+
+export default UserList
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,25 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+// import { StrictMode } from 'react'
+// import { createRoot } from 'react-dom/client'
+// // import './index.css'
+// import App from './App.jsx'
+// import "bulma/css/bulma.css"
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
+// createRoot(document.getElementById('root')).render(
+//   <StrictMode>
+//     <App />
+//   </StrictMode>,
+// )
+
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import 'bulma/css/bulma.css';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
     <App />
-  </StrictMode>,
-)
+  </React.StrictMode>
+);
+
+
